9390b911140171f26c6f4e4db8cc53784674bde1
rccoder <rccoder.net@gmail.com>
2016-10-17 15:53:35 +0800

    update spider

--- spider/index.js
+++ spider/index.js
@@ -3,6 +3,8 @@
 const request = require('superagent')
 const cheerio = require('cheerio')
 
+const base_url = 'http://github.com/'
+
 const data = []
 request
   .get('https://github.com/rccoder/blog/issues')
@@ -12,9 +14,10 @@ request
     } else {
       let $ = cheerio.load(res.text)
       $('.Box-row-link').each((id, value) => {
-          console.log(id)
-          console.log(value)
+        let url = `${base_url}${value.attribs.href}`
+        let title = `${value.children[0].data.trim()}`
+        let label = []
+        label.push()
       })
-
     }
   })
